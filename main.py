6bd1a652aaba93ba050f8731c5f1638e351de970
Nsja2806 <nizambranom@unal.edu.co>
2024-02-18 23:04:53 -0500

    Creacion codigo py

---
+++ main.py
@@ -0,0 +1,51 @@
+class Asientos:
+    def __init__(self,color,precio,registro):
+        self.color=color
+        self.precio=precio
+        self.registro=registro
+    
+    def cambiarColor(self,color):
+        if ((color=="amarillo") or (color=="verde") or (color=="negro") or (color=="blanco") or (color=="rojo")):
+            self.color=color
+
+class Auto:
+    cantidadCreados=0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo=modelo
+        self.precio=precio
+        self.precio=asientos
+        self.marca=marca
+        self.motor=motor
+        self.registro=registro
+        
+
+    def cantidadAsientos(self):
+        ca=00 #se define la variable ca como cantidad de asientos
+        for i in self.asientos:
+            if i!=None:
+                ca+=1
+        return ca
+
+    def verificarIntegridad(self):
+        if (self.registro!=self.motor.registro):
+            return "Las piezas no son originales"
+        for i in self.asientos:
+            if (self.registro!=i.registro):
+                return "Las piezas no son originales"
+        return "Auto original"
+    
+class Motor:
+    def __init__(self,numeroCilindros,tipo, registro):
+        self.numeroCilindros=numeroCilindros
+        self.tipo=tipo
+        self.registro=registro
+    
+    def cambiarRegistro(self,registro):
+        self.registro=registro
+    
+    def asignarTipo(self,tipo):
+        if ((tipo=="electrico") or (tipo=="gasolina")):
+            self.tipo=tipo
+        
+
+            
